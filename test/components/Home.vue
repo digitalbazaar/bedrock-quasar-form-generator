5610eaa00eaba053b38904b9d631f8f08e02e39e
Dave Longley <dlongley@digitalbazaar.com>
2019-03-06 13:43:59 -0500

    Add core files.

---
+++ test/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page
+    class="column gutter-md"
+    padding>
+    <div class="column items-center">
+      <div>
+        <br-q-form-generator />
+      </div>
+    </div>
+  </q-page>
+</template>
+<script>
+/*!
+ * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+import {BrQFormGenerator} from 'bedrock-quasar-form-generator';
+
+export default {
+  name: 'Home',
+  components: {BrQFormGenerator},
+  data() {
+    return {};
+  }
+};
+</script>
+<style>
+</style>
