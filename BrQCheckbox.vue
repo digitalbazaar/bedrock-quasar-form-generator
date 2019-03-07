49f9618a835b84c468f9ece8869e4c3ad5c30381
Dave Longley <dlongley@digitalbazaar.com>
2019-03-07 10:17:30 -0500

    Fix copyright headers.

--- BrQCheckbox.vue
+++ BrQCheckbox.vue
@@ -11,7 +11,7 @@
 </template>
 <script>
 /*!
- * Copyright (c) 2018 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
 
--- BrQInput.vue
+++ BrQInput.vue
@@ -16,7 +16,7 @@
 </template>
 <script>
 /*!
- * Copyright (c) 2018 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
 
